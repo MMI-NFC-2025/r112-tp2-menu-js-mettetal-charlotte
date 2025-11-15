3a4c7498e13c9c92dfefb9e6d7eb9a12dec999ad
mettetal-charlotte <charlotte.mettetal@edu.univ-fcomte.fr>
2025-11-15 12:21:05 +0100

    classe noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,11 @@
 const toggle = document.querySelector('.menu-btn');
 const nav = document.querySelector(".menu")
-const isOpen = toggle.ariaExpanded === "true";
-const isClosed = !isOpen;
-console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+
 toggle.addEventListener('click', () => {
-    nav.classList.toggle('menu--open');
-    nav.ariaHidden = "false";
-    toggle.ariaExpanded = "true";
-    toggle.addEventListener('click', () => {
-        const isOpen = toggle.ariaExpanded === "true";
-        const isClosed = !isOpen;
-        console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
-        nav.ariaHidden = "true";
-        toggle.ariaExpanded = "false";
-    });
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    nav.ariaHidden = isOpen;
+    toggle.ariaExpanded = isClosed;
+    document.body.classList.toggle("noscroll");
 });
